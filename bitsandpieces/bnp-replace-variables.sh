cffcd1f6a1fdb2f010806e10721cc2a5d6af6ba8
Pjotr Savitski <pjotr.savitski@gmail.com>
2015-11-13 15:26:05 +0200

    Fixed config replace locations to correct ones.
    The last remaining issue should be an automatic setup and registration
    of the client.

--- bitsandpieces/bnp-replace-variables.sh
+++ bitsandpieces/bnp-replace-variables.sh
@@ -4,10 +4,10 @@
 cd /usr/share/nginx/html/js/config
 
 # Fill cnfiguration file as needed
-sed -i "s#SSS_HOST_REST#${BASE_URL}/sss#g" config.json
-sed -i "s#OIDC_AUTHORIZATION_URL#${BASE_URL}/o/oauth2/authorize#g" config.json
+sed -i "s#SSS_HOST_REST#${LAYERS_API_URI}sss.adapter.rest.v2/#g" config.json
+sed -i "s#OIDC_AUTHORIZATION_URL#${LAYERS_API_URI}o/oauth2/authorize#g" config.json
 sed -i "s#OIDC_CLIENT_ID#NOT_IMPLEMENTED_YET#g" config.json
 sed -i "s#AFFECT_URL##g" config.json
 sed -i "s#HELP_URL##g" config.json
-sed -i "s#DISCUSSION_TOOL_URL#${BASE_URL}/discussiontool#g" config.json
+sed -i "s#DISCUSSION_TOOL_URL#${LAYERS_API_URI}discussiontool/#g" config.json
 
