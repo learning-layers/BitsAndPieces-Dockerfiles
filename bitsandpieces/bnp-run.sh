02b626f40ffb557f6e4f702f040892b4ae721651
Pjotr Savitski <pjotr.savitski@gmail.com>
2016-01-22 14:20:05 +0200

    A check to only run config file replacement procedure once.
    A .lock file is created once replacements have been made. Every
    consequent restart no longer tries to run the replacement procedure.

--- bitsandpieces/bnp-run.sh
+++ bitsandpieces/bnp-run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Replace variables if needed
-source ~/bin/bnp-replace-variables.sh
+if [ ! -f bnp-replacements-made.lock ];
+then
+	source ~/bin/bnp-replace-variables.sh
+	touch bnp-replacements-made.lock
+fi
 
 # Start nginx
 sudo /usr/sbin/nginx
