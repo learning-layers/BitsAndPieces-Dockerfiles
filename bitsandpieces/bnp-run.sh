c55b01ab7dcac06a96d1e3f97ae71142af25d947
Pjotr Savitski <pjotr.savitski@gmail.com>
2016-01-22 21:15:53 +0200

    Samll fix to .lock file location.

--- bitsandpieces/bnp-run.sh
+++ bitsandpieces/bnp-run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Replace variables if needed
+initialScriptLocation=$PWD
 if [ ! -f bnp-replacements-made.lock ];
 then
 	source ~/bin/bnp-replace-variables.sh
+	cd $initialScriptLocation
 	touch bnp-replacements-made.lock
 fi
 
